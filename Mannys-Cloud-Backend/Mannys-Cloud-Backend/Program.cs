e7341a912556a19c30dba679eeb3d718d02a013d
Emmanuel-Castillo <emmanuel.2001alonso.ec@gmail.com>
2025-10-03 11:59:09 -0500

    Add Vercel link to CORS policy

--- Mannys-Cloud-Backend/Mannys-Cloud-Backend/Program.cs
+++ Mannys-Cloud-Backend/Mannys-Cloud-Backend/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://localhost:5173")
+                          policy.WithOrigins("http://localhost:5173", "https://mannys-cloud-q031v1q8w-emmanuelcastillos-projects.vercel.app")
                             .AllowAnyHeader()
                             .AllowAnyMethod()
                             .AllowCredentials();
